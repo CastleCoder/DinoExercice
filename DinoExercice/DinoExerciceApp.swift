40b422c5d03c643a7e153fdc1c40544e419623ce
Cyrille CHATEAU <cyrillechateau@icloud.com>
2023-12-17 16:13:13 +0100

    Initial Commit

---
+++ DinoExercice/DinoExerciceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinoExerciceApp.swift
+//  DinoExercice
+//
+//  Created by Cyrille Chateau on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DinoExerciceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
